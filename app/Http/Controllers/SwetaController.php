22a133e716d2d64d168f39a7236e8b3276b162ad
swetaTamrakar <Tamrakarsweta@gmail.com>
2018-05-31 12:35:39 +0545

    message

---
+++ app/Http/Controllers/SwetaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SwetaController extends Controller
+{
+    public function index(){
+    	return view('presentation');
+    	public function second(){
+    	return view('laracasts');
+    
+
+}
+
+
+
---
+++ resources/views/random.blade.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+<h1>SWETA</h1> 
+	</body>
+	</html>	
